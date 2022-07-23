ac2d9173a7246f3cf36da3927d8e7ab63f5ed0cb
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2022-07-24 01:28:22 +0200

    Update first_boot.sh

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -7,3 +7,15 @@ apt-get install -qy $(cat "toolbox/pkgs/ubuntu-jammy.txt")
 
 # Install packages via PyPI
 pip3 install meson ninja pyelftools
+
+# configure xNVMe and build dependencies (fio, liburing, and SPDK)
+meson setup builddir
+
+# build xNVMe
+meson compile -C builddir
+
+# install xNVMe
+meson install -C builddir
+
+# uninstall xNVMe
+# cd builddir && meson --internal uninstall
