74c741b305ef4657e97d75499110095060eabf55
Rickard Lundin <rickard@x14.se>
2022-07-24 14:37:16 +0000

    works to build and install xnvme

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -1,5 +1,5 @@
 #
-#
+# xnvme 0.4.0
 #
 
 # Install packages via apt-get
@@ -8,6 +8,9 @@ apt-get install -qy $(cat "toolbox/pkgs/ubuntu-jammy.txt")
 # Install packages via PyPI
 pip3 install meson ninja pyelftools
 
+
+git clone https://github.com/OpenMPDK/xNVMe.git xnvme
+cd xnvme
 # configure xNVMe and build dependencies (fio, liburing, and SPDK)
 meson setup builddir
 
@@ -15,7 +18,7 @@ meson setup builddir
 meson compile -C builddir
 
 # install xNVMe
-meson install -C builddir
+sudo meson install -C builddir
 
 # uninstall xNVMe
 # cd builddir && meson --internal uninstall
