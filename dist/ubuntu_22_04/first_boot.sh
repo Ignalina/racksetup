a86c34b79ba195bab28f447ba2a224714addf6ed
Rickard Lundin <rickard@x14.se>
2022-08-08 20:23:47 +0000

    more stuff added

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -27,12 +27,16 @@ sudo meson install -C builddir
 #
 # Mellanox
 # warning: below update only works if firmware not too old due to changed nvidia nameing
-cd ..
-wget https://www.mellanox.com/downloads/firmware/mlxup/4.20.0/SFX/linux_x64/mlxup
+#cd ..
+#wget https://www.mellanox.com/downloads/firmware/mlxup/4.20.0/SFX/linux_x64/mlxup
 #./mlxup --download-default -y --download-os Linux_x64 --download-type self_extractor
-chmod +x mlxsup
-sudo ./mlxup --online -y -u
-
+#chmod +x mlxsup
+#sudo ./mlxup --online -y -u
+wget https://www.mellanox.com/downloads/MFT/mft-4.21.0-99-x86_64-deb.tgz
+tar -zxf mft-4.21.0-99-x86_64-deb.tgz
+cd mft-4.21.0-99-x86_64-deb$ 
+sudo ./install.sh
+cd ..
 #wget mlnx-en-5.6-2.0.9.0-ubuntu22.04-x86_64.tgz
 tar -zxf mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
 cd mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64
