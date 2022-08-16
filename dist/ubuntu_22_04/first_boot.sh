05736c049db282f502801b0cd6ece40352a291b4
Rickard Lundin <rickard@x14.se>
2022-08-16 00:04:53 +0000

    ok

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -3,7 +3,7 @@
 #
 
 # Install packages via apt-get
-apt-get install -qy $(cat "toolbox/pkgs/ubuntu-jammy.txt")
+sudo apt-get install -qy $(cat "toolbox/pkgs/ubuntu-jammy.txt")
 
 # Install packages via PyPI
 pip3 install meson ninja pyelftools
