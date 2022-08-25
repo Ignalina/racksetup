b4f30b2db166e8d4cf7946ecd54cfce33d6a2338
Rickard Lundin <rickard@x14.se>
2022-08-25 20:06:03 +0000

    fix scylladb

--- dist/ubuntu_22_04/install_scylladb.sh
+++ dist/ubuntu_22_04/install_scylladb.sh
@@ -2,9 +2,10 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys d0a112e06
 sudo wget -O /etc/apt/sources.list.d/scylla.list http://downloads.scylladb.com/deb/debian/scylla-5.0.list
 sudo apt-get update
 sudo apt-get install -y scylla 
-sudo scylla_setup --no-raid-setup --online-discard --nic eth2 \
-      --no-coredump-setup --io-setup 1 --no-memory-setup \
-      --no-rsyslog-setup
+sudo scylla_setup --disks /dev/nvme1n1 --online-discard --nic eth2 \
+                 --no-kernel-check --no-verify-package \
+                 --no-coredump-setup --io-setup 1 --no-memory-setup \
+                 --no-rsyslog-setup
 
 #sudo apt-get update
 #sudo apt-get install -y openjdk-8-jre-headless
