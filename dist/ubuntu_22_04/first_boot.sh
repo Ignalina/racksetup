bdc1f12d81607635a65a84dc82234ad0e27a703a
Rickard Ernst Björn <rickard@ignalina.dk>
2023-01-03 23:30:43 +0100

    ok

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -1,7 +1,13 @@
 sudo apt-get install -y nano git git-lfs
+# DONT FORGET TO SET AFTER UPDATE OR NEW CARD 
+# lspci | grep -i mellanox
+# sudo mstconfig -d 41:00.0 set LINK_TYPE_P1=2
+
 
 #
 # Mellanox
+sudo apt-get install -y gcc make dkms python2
+
 wget https://www.mellanox.com/downloads/MFT/mft-4.22.1-11-x86_64-deb.tgz
 tar -zxf mft-4.22.1-11-x86_64-deb.tgz
 cd mft-4.22.1-11-x86_64-deb
@@ -10,8 +16,8 @@ cd ..
 #wget mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
 tar -zxf MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64.tgz
 cd MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64
-sudo apt-get install dkms python2
-sudo ./install --eth-only
+sudo ./mlnxofedinstall --eth-only
+# to load directly /etc/init.d/mlnx-en.d restart
 
 # sudo mlnx_tune <-- gives info
 
