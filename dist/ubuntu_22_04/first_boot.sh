5e1bcc7af136f072617377a78339a8958427341f
Rickard Ernst Björn <rickard@ignalina.dk>
2023-01-03 20:43:44 +0100

    upgraded network drivers

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -1,45 +1,15 @@
-#
-# xnvme 0.4.0
-#
-
-# Install packages via apt-get
-sudo apt-get install -qy $(cat "toolbox/pkgs/ubuntu-jammy.txt")
-
-# Install packages via PyPI
-pip3 install meson ninja pyelftools
-
-
-git clone https://github.com/OpenMPDK/xNVMe.git xnvme
-cd xnvme
-# configure xNVMe and build dependencies (fio, liburing, and SPDK)
-meson setup builddir
-
-# build xNVMe
-meson compile -C builddir
-
-# install xNVMe
-sudo meson install -C builddir
-
-# uninstall xNVMe
-# cd builddir && meson --internal uninstall
-
+sudo apt-get install -y nano git git-lfs
 
 #
 # Mellanox
-# warning: below update only works if firmware not too old due to changed nvidia nameing
-#cd ..
-#wget https://www.mellanox.com/downloads/firmware/mlxup/4.20.0/SFX/linux_x64/mlxup
-#./mlxup --download-default -y --download-os Linux_x64 --download-type self_extractor
-#chmod +x mlxsup
-#sudo ./mlxup --online -y -u
-wget https://www.mellanox.com/downloads/MFT/mft-4.21.0-99-x86_64-deb.tgz
-tar -zxf mft-4.21.0-99-x86_64-deb.tgz
-cd mft-4.21.0-99-x86_64-deb
+wget https://www.mellanox.com/downloads/MFT/mft-4.22.1-11-x86_64-deb.tgz
+tar -zxf mft-4.22.1-11-x86_64-deb.tgz
+cd mft-4.22.1-11-x86_64-deb
 sudo ./install.sh
 cd ..
-wget mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
-tar -zxf mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
-cd mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64
+#wget mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
+tar -zxf MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64.tgz
+cd MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64
 sudo apt-get install dkms python2
 sudo ./install --eth-only
 
