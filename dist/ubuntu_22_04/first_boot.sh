735314c751c1bbf29e10a11cfbe100fdb8911a91
Rickard Lundin <rickard@ignalina.dk>
2023-01-04 13:21:47 +0000

    revert back to ofed 5.7

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -13,10 +13,10 @@ tar -zxf mft-4.22.1-11-x86_64-deb.tgz
 cd mft-4.22.1-11-x86_64-deb
 sudo ./install.sh
 cd ..
-#wget mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
-tar -zxf MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64.tgz
-cd MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64
-sudo ./mlnxofedinstall --eth-only
+wget https://www.mellanox.com/downloads/ofed/MLNX_EN-5.7-1.0.2.0/mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
+tar -zxf MLNX_OFED_LINUX-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
+cd MLNX_OFED_LINUX-5.7-1.0.2.0-ubuntu22.04-x86_64
+sudo ./install --eth-only
 # to load directly /etc/init.d/mlnx-en.d restart
 
 # sudo mlnx_tune <-- gives info
