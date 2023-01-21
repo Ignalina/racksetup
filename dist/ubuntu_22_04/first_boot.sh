d6443be329001b56e4e3bc2983c37fa9c2892622
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-21 16:36:34 +0100

    Update first_boot.sh

--- dist/ubuntu_22_04/first_boot.sh
+++ dist/ubuntu_22_04/first_boot.sh
@@ -1,35 +1,21 @@
-sudo apt-get install -y nano git git-lfs
-# DONT FORGET TO SET AFTER UPDATE OR NEW CARD 
-# lspci | grep -i mellanox
-# sudo mstconfig -d 41:00.0 set LINK_TYPE_P1=2
+# before cloning and running git is needed by "sudo apt-get install -y nano git git-lfs"
 
 
-#
-# Mellanox
-sudo apt-get install -y gcc make dkms python2
+mkfs.ext4 -F /dev/md0
+mkdir /mnt/newvar
+mount  /dev/md0 /mnt/newvar
 
-wget https://www.mellanox.com/downloads/MFT/mft-4.22.1-11-x86_64-deb.tgz
-tar -zxf mft-4.22.1-11-x86_64-deb.tgz
-cd mft-4.22.1-11-x86_64-deb
-sudo ./install.sh
-cd ..
-wget https://www.mellanox.com/downloads/ofed/MLNX_EN-5.7-1.0.2.0/mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
-tar -zxf mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
-cd mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64
-sudo ./install --eth-only
+# move existing var and create x14 
+init 1
+cp -apx /var/* /mnt/newvar
+rm -rf /var/*
+mkdir -p /mnt/newvar/lib/x14
+umount /mnt/newvar
 
+# create
+mkdir /usr/lib/x14
 
-# to load directly /etc/init.d/mlnx-en.d restart
-
-# sudo mlnx_tune <-- gives info
+reboot
 
 
-# For ubuntu , turn back to  interfaces
 
-source revert.sh
-
-## OBSELETE use raid md0 instead
-# Mount 1735 main storage
-#sudo mkfs.ext4 -f /dev/nvme1n1
-##mkdir -p /nvme/s1735_1
-##mount /dev/nvme1n1 /nvme/s1735_1
