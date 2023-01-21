fac17aca288670e100cb79a1b23628f8609021bf
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-21 18:03:22 +0100

    Update install_mellanox.sh

--- dist/ubuntu_22_04/install_mellanox.sh
+++ dist/ubuntu_22_04/install_mellanox.sh
@@ -5,17 +5,18 @@
 
 #
 # Mellanox
-sudo apt-get install -y gcc make dkms python2
+export NEEDRESTART_MODE=a
+apt-get install -y gcc make dkms python2
 
 wget https://www.mellanox.com/downloads/MFT/mft-4.22.1-11-x86_64-deb.tgz
 tar -zxf mft-4.22.1-11-x86_64-deb.tgz
 cd mft-4.22.1-11-x86_64-deb
-sudo ./install.sh
+./install.sh
 cd ..
 wget https://www.mellanox.com/downloads/ofed/MLNX_EN-5.7-1.0.2.0/mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
 tar -zxf mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64.tgz
 cd mlnx-en-5.7-1.0.2.0-ubuntu22.04-x86_64
-sudo ./install --eth-only
+./install --eth-only
 
 
 # to load directly /etc/init.d/mlnx-en.d restart
@@ -24,5 +25,5 @@ sudo ./install --eth-only
 
 
 # For ubuntu , turn back to  interfaces
-
+cd ..
 source revert.sh
