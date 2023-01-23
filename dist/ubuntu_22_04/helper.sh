cda654b31850d9675dc2dcd5a843f1c879248af2
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-23 22:56:21 +0100

    Update helper.sh

--- dist/ubuntu_22_04/helper.sh
+++ dist/ubuntu_22_04/helper.sh
@@ -1,9 +1,11 @@
-
-
 mesh_machine () {
-  commands
   h=$(hostname)
   interface=interfaces${h: -1}
   return ${interface}
 }
 
+mesh_machine_nr () {
+  h=$(hostname)
+  nr=${h: -1}
+  return ${nr}
+}
