b1efc7d0ae1ac5ea5967e0560ec75a8c34d37c5a
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-23 22:52:01 +0100

    Update install_spark.sh

--- dist/ubuntu_22_04/install_spark.sh
+++ dist/ubuntu_22_04/install_spark.sh
@@ -1,7 +1,9 @@
+source helper.sh
+
 apt-get install -y openjdk-17-jdk-headless zip
 useradd -s /sbin/nologin -M spark -G x14
 
-cd /usr/lib/x14
+pushd /usr/lib/x14
 mkdir spark
 pushd spark
 curl -s "https://get.sdkman.io" | bash
@@ -11,14 +13,17 @@ sdk install scala 2.13.8
 wget https://dlcdn.apache.org/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3-scala2.13.tgz
 tar -zxf spark-3.3.1-bin-hadoop3-scala2.13.tgz
 pushd spark-3.3.1-bin-hadoop3-scala2.13
-
 # todo copy to env file
-
 popd
+popd 
 
 # TODO in case of first node only
-cp dist/ubuntu_22_04/systemd/spark-master.service /etc/systemd/system/
-systemctl enable spark-master
+mesh_machine_nr
+if [[ $? -eq 1 ]]
+then 
+    cp systemd/spark-master.service /etc/systemd/system/
+    systemctl enable spark-master
+fi
 
-cp dist/ubuntu_22_04/systemd/spark-slave.service /etc/systemd/system/
+cp systemd/spark-slave.service /etc/systemd/system/
 systemctl enable spark-slave
