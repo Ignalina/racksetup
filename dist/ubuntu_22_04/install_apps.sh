686f8281663c5aa50289630c1916f5092b36c163
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-29 18:52:51 +0100

    Update install_apps.sh

--- dist/ubuntu_22_04/install_apps.sh
+++ dist/ubuntu_22_04/install_apps.sh
@@ -1,18 +1,8 @@
-
+# Needs be run from dist/ubuntu_22_04 directory
 
 #source installation_kits/install_nginx.sh
-#pushd installation_kits/nginx
-#popd
-
 #source installation_kits/ballista/install_ballista.sh
-#pushd installation_kits/ballista
-#popd
-
 #source installation_kits/spark/install_spark.sh
-#pushd installation_kits/spark
-#popd
 
 source installation_kits/minio/install_minio.sh
-pushd installation_kits/minio
-popd
 
