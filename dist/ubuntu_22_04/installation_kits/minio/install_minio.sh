1d8eaf2290230202bafa2a73fc12984082f951b6
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-29 19:07:52 +0100

    Update install_minio.sh

--- dist/ubuntu_22_04/installation_kits/minio/install_minio.sh
+++ dist/ubuntu_22_04/installation_kits/minio/install_minio.sh
@@ -2,11 +2,14 @@ pushd installation_kits/minio
 wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio_20230125001954.0.0_amd64.deb -O minio.deb
 sudo dpkg -i minio.deb
 
-mkdir -p /var/lib/x14/minio/disk1
+mkdir -p /var/lib/x14/minio/mnt//disk1
+mkdir -p /var/lib/x14/minio/mnt//disk2
+mkdir -p /var/lib/x14/minio/mnt//disk3
+mkdir -p /var/lib/x14/minio/mnt//disk4
 
 groupadd -r minio-user
 useradd -M -r -g minio-user minio-user
-chown minio-user:minio-user /var/lib/x14/minio/disk1
+chown -R minio-user:minio-user /var/lib/x14/minio/mnt
 
 cp default.minio /etc/default/minio
 
