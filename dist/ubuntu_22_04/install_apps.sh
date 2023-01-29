978766fa543d74a0e835e89cdcc2f59082835ee6
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-01-29 18:39:05 +0100

    Create install_apps.sh

---
+++ dist/ubuntu_22_04/install_apps.sh
@@ -0,0 +1,18 @@
+
+
+#pushd installation_kits/nginx
+#source install_nginx.sh
+#popd
+
+#pushd installation_kits/ballista
+#source install_ballista.sh
+#popd
+
+#pushd installation_kits/spark
+#source install_spark.sh
+#popd
+
+pushd installation_kits/minio
+source install_minio.sh
+popd
+
