b124dcc6dca289ebb8b16f07398939dbd24e2f07
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-03-05 17:38:38 +0100

    fix spelling mistake

--- dist/ubuntu_22_04/installation_kits/datadisk/install.sh
+++ dist/ubuntu_22_04/installation_kits/datadisk/install.sh
@@ -1,4 +1,4 @@
-mdadm --create --verbose /dev/md0 --level=0 /dev/${brokkr_md0_device[0]} /dev/${brokr_md0_device[1]}
+mdadm --create --verbose /dev/md0 --level=0 /dev/${brokkr_md0_device[0]} /dev/${brokkr_md0_device[1]}
 mdadm --detail --scan >> /etc/mdadm.conf
 echo "/dev/md0 /var xfs defaults 0 1" >> /etc/fstab
 
