21804b1095ea71abe56617087985dda6e7b00ae8
rickard <rickard@ignalina.dk>
2023-03-05 17:48:33 +0000

    fix

--- dist/ubuntu_22_04/installation_kits/revert/install.sh
+++ dist/ubuntu_22_04/installation_kits/revert/install.sh
@@ -14,9 +14,8 @@ interface=interfaces${h: -1}
 
 $brokkr_gotplexe ${interface}.tpl --set eth0=${brokkr_mesh_interface_name["eth0"]} --set eth1=${brokkr_mesh_interface_name["eth1"]} --set eth2=${brokkr_mesh_interface_name["eth2"]} --set eth3=${brokkr_mesh_interface_name["eth3"]} > /etc/network/interfaces
 
-eth0=${brokkr_mesh_interface_name["eth0"]}
-ifdown --force ${eth0}
-ifup ${eth0}
+ifdown --force eth0
+ifup eth0
 
 unlink /etc/resolv.conf
 echo nameserver 8.8.8.8 >> /etc/resolv.conf
