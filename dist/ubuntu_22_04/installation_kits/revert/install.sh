732353b86bd1fbb42c8d2c042409189c7daa466e
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-03-14 22:14:12 +0100

    fix software support for vmbr0

--- dist/ubuntu_22_04/installation_kits/revert/install.sh
+++ dist/ubuntu_22_04/installation_kits/revert/install.sh
@@ -3,7 +3,7 @@
 # For ubuntu , turn back to  interfaces
 export NEEDRESTART_MODE=a
 apt update
-apt install -y ifupdown net-tools
+apt install -y ifupdown net-tools bridge-utils
 cp grub /etc/default/grub
 update-grub
 #
