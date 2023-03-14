cd24bd1de8ac58aba8fc4195a4e1fc8703f1699b
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-03-14 22:02:07 +0100

    persist device name to ramfs

--- dist/ubuntu_22_04/installation_kits/datadisk/install.sh
+++ dist/ubuntu_22_04/installation_kits/datadisk/install.sh
@@ -1,5 +1,6 @@
 mdadm --create --verbose /dev/md0 --raid-devices=2 --level=0 /dev/${brokkr_md0_device[0]} /dev/${brokkr_md0_device[1]}
 mdadm --detail --scan >> /etc/mdadm.conf
 echo "/dev/md0 /var xfs defaults 0 1" >> /etc/fstab
+update-initramfs -u
 
  
