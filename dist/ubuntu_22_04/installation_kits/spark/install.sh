07bb51633369bcc74b822dab119a381de1c662fa
Rickard Lundin <rickard@x14.se>
2023-03-16 17:19:54 +0100

    scala separated from spark

--- dist/ubuntu_22_04/installation_kits/spark/install.sh
+++ dist/ubuntu_22_04/installation_kits/spark/install.sh
@@ -6,9 +6,6 @@ useradd -s /sbin/nologin -M spark -G x14
 pushd /usr/lib/x14
 mkdir spark
 pushd spark
-curl -s "https://get.sdkman.io" | bash
-source /root/.sdkman/bin/sdkman-init.sh
-sdk install scala 2.13.8
 
 wget https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3-scala2.13.tgz
 tar -zxf spark-3.3.2-bin-hadoop3-scala2.13.tgz
