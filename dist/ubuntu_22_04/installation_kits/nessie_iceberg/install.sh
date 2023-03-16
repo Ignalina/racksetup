00f61e3ded1d93222b079bd52118d61b4279bb95
rickard <rickard@ignalina.dk>
2023-03-16 23:53:39 +0000

    fix nessie

--- dist/ubuntu_22_04/installation_kits/nessie_iceberg/install.sh
+++ dist/ubuntu_22_04/installation_kits/nessie_iceberg/install.sh
@@ -8,20 +8,28 @@ then
 
 
    apt-get -y install rocksdb-tools ant
+   cp build.xml /tmp
+   cp ivy.xml /tmp
    pushd /usr/lib/x14
-   wget https://dlcdn.apache.org//ant/ivy/2.5.1/apache-ivy-2.5.1-bin-with-deps.zip
-   unzip apache-ivy-2.5.1-bin-with-deps.zip
-   ant resolve
-   cp lib/*.jar /usr/lib/x14/spark/jar/
-
+  
    mkdir nessie
    pushd nessie
+
+   wget https://dlcdn.apache.org//ant/ivy/2.5.1/apache-ivy-2.5.1-bin-with-deps.zip
+   unzip apache-ivy-2.5.1-bin-with-deps.zip
+   cp /tmp/build.xml .
+   cp /tmp/ivy.xml .
+   ant resolve
+   cp lib/*.jar /usr/lib/x14/spark/spark-3.3.2-bin-hadoop3-scala2.13/jars/
+
+
    wget https://github.com/projectnessie/nessie/releases/download/nessie-0.51.1/nessie-quarkus-0.51.1-runner
    chmod +x
+   popd
+   popd
    cp nessie.service /etc/systemd/system/
    systemctl enable nessie
-   popd
-   popd
+
    chown -R nessie:x14 /usr/lib/x14/nessie
    systemctl start nessie
 
