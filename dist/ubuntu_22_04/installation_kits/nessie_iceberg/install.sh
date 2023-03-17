3104624e19fab6cbe4f06592ca19d860c11110cf
rickard <rickard@ignalina.dk>
2023-03-17 00:44:06 +0000

    add config

--- dist/ubuntu_22_04/installation_kits/nessie_iceberg/install.sh
+++ dist/ubuntu_22_04/installation_kits/nessie_iceberg/install.sh
@@ -11,7 +11,7 @@ then
    cp build.xml /tmp
    cp ivy.xml /tmp
    pushd /usr/lib/x14
-  
+
    mkdir nessie
    pushd nessie
 
@@ -20,17 +20,49 @@ then
    cp /tmp/build.xml .
    cp /tmp/ivy.xml .
    ant resolve
+   
    cp lib/*.jar /usr/lib/x14/spark/spark-3.3.2-bin-hadoop3-scala2.13/jars/
+   pushd /usr/lib/x14/spark/spark-3.3.2-bin-hadoop3-scala2.13/
+   chown -R spark:x14 jars/
 
+#spark-sql --packages "org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.0.0,org.projectnessie:nessie-spark-extensions-3.3_2.12:0.44.0,software.amazon.awssdk:bundle:2.17.178,software.amazon.awssdk:url-connection-client:2.17.178" \
+#spark-sql --packages "org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.0.0,org.projectnessie:nessie-spark-extensions-3.3_2.12:0.44.0,software.amazon.awssdk:bundle:2.17.178,software.amazon.awssdk:url-connection-client:2.17.178" \
+#--conf spark.sql.extensions="org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions" \
+#--conf spark.sql.catalog.nessie.uri=$NESSIE_URI \
+#--conf spark.sql.catalog.nessie.ref=main \
+#--conf spark.sql.catalog.nessie.authentication.type=BEARER \
+#--conf spark.sql.catalog.nessie.authentication.token=$TOKEN \
+#--conf spark.sql.catalog.nessie.catalog-impl=org.apache.iceberg.nessie.NessieCatalog \
+#--conf spark.sql.catalog.nessie.warehouse=$WAREHOUSE \
+#--conf spark.sql.catalog.nessie=org.apache.iceberg.spark.SparkCatalog \
+#--conf spark.sql.catalog.nessie.io-impl=org.apache.iceberg.aws.s3.S3FileIO
+
+   echo 'spark.sql.extensions="org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions"' >> con/conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie.uri=$NESSIE_URI' >> con/conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie.ref=main' >> con/conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie.catalog-impl=org.apache.iceberg.nessie.NessieCatalog' >> con/conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie.warehouse=$WAREHOUSE' >> con/conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie=org.apache.iceberg.spark.SparkCatalog' >> con/conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie.io-impl=org.apache.iceberg.aws.s3.S3FileIO' >> con/conf/spark-defaults.conf
+
+
+   echo '' >> con/conf/spark-defaults.conf
+
+
+   popd
 
    wget https://github.com/projectnessie/nessie/releases/download/nessie-0.51.1/nessie-quarkus-0.51.1-runner
    chmod +x nessie-quarkus-0.51.1-runner
+
    popd
    popd
    cp nessie.service /etc/systemd/system/
    systemctl enable nessie
 
    chown -R nessie:x14 /usr/lib/x14/nessie
+
+
+
    systemctl stop spark
    systemctl start nessie
    systemctl start spark
