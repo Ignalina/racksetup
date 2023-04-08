599333cc4e896b9d495f1b59a7c73f582ccba81c
root <root@r9v1.x14.se>
2023-04-09 01:17:36 +0200

    fix

--- dist/rocky_9_1/installation_kits/nessie_iceberg/start-spark-sql.sh
+++ dist/rocky_9_1/installation_kits/nessie_iceberg/start-spark-sql.sh
@@ -2,6 +2,6 @@ export AWS_ACCESS_KEY_ID=labb
 export AWS_SECRET_ACCESS_KEY=password
 export AWS_REGION=us-east-1
 
-/usr/lib/x14/spark/spark-3.3.2-bin-hadoop3/bin/spark-sql --master spark://10.1.1.93:7077 --packages org.projectnessie:nessie-spark-extensions-3.3_2.12:0.54.0,org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.2.0,software.amazon.awssdk:sts:2.20.18,software.amazon.awssdk:s3:2.20.18,software.amazon.awssdk:url-conne>
+/usr/lib/x14/spark/spark-3.3.2-bin-hadoop3/bin/spark-sql --master spark://10.1.1.93:7077 --packages org.projectnessie.nessie-integrations:nessie-spark-extensions-3.3_2.12:0.54.0,org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.2.0,software.amazon.awssdk:sts:2.20.18,software.amazon.awssdk:s3:2.20.18,software.amazon.awssdk:url-conne>
 # use nessie;
 # CREATE TABLE demo3 (id bigint, data string) ;
