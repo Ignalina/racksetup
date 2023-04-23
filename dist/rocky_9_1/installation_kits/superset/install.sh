d1ee0e29f40adb98ab719b3154906ff8d93cfce8
root <root@r9v1.x14.se>
2023-04-23 22:35:53 +0200

    superset

--- dist/rocky_9_1/installation_kits/superset/install.sh
+++ dist/rocky_9_1/installation_kits/superset/install.sh
@@ -4,23 +4,27 @@ if [[ $nr -eq 1 ]]
 then
    echo "I AM MASTER_HOST=${brokkr_mesh_ip[1]}"
 
-   dnf -y install gcc gcc-c++ libffi-devel python3-devel python3-pip python3-wheel openssl-devel cyrus-sasl-devel openldap-devel
+   dnf -y install gcc gcc-c++ libffi-devel python3-devel python3-pip python3-pip-wheel  openssl-devel cyrus-sasl-devel openldap-devel g++
    pip3 install --upgrade pip
    pip install --upgrade setuptools pip
-   pip -y install virtualenv
+   pip install virtualenv
 
 
    useradd -s /sbin/nologin -M superset -G x14
    cp superset.service /tmp
+   cp start.sh /tmp
    pushd /usr/lib/x14
      mkdir -p /var/lib/x14/superset/
      chown -R zeppelin:x14 /var/lib/x14/superset/
      mkdir -p superset
      chown -R superset:x14 superset
        pushd superset
+       cp /tmp/start.sh .
+       chmod +x start.sh
        python3 -m venv venv
        . venv/bin/activate
-       pip install apache-superset
+       python3 -m pip install --upgrade pip
+       pip install numpy apache-superset pillow
        superset db upgrade
 
 
@@ -46,6 +50,6 @@ then
        systemctl enable superset.service
          
        popd
-       chown -R superset:x14
+       chown -R superset:x14  superset
    popd
 fi
