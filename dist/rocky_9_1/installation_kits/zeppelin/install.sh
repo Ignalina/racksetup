e83f1c144fbb5630353edbc515adc96ed6c53fa8
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-05-06 11:21:53 +0200

    Update install.sh

--- dist/rocky_9_1/installation_kits/zeppelin/install.sh
+++ dist/rocky_9_1/installation_kits/zeppelin/install.sh
@@ -20,17 +20,17 @@ then
        wget https://dlcdn.apache.org/zeppelin/zeppelin-0.10.1/zeppelin-0.10.1-bin-all.tgz
        tar -zxf zeppelin-0.10.1-bin-all.tgz
          pushd zeppelin-0.10.1-bin-all
-         echo "export AWS_ACCESS_KEY_ID=labb" >> conf/zeppeline-env.sh
-	 echo "export AWS_SECRET_ACCESS_KEY=password" >> conf/zeppeline-env.sh
-         echo "export AWS_REGION=us-east-1" >>  conf/zeppeline-env.sh
-         echo "export ZEPPELIN_SPARK_ENABLESUPPORTEDVERSIONCHECK=false" >> conf/zeppeline-env.sh
-         echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.362.b09-2.el9_1.x86_64/jre" >> conf/zeppeline-env.sh
-         echo "export SPARK_MASTER=spark://10.1.1.93:7077" >> conf/zeppeline-env.sh
+         echo "export AWS_ACCESS_KEY_ID=labb" >> conf/zeppelin-env.sh
+	 echo "export AWS_SECRET_ACCESS_KEY=password" >> conf/zeppelin-env.sh
+         echo "export AWS_REGION=us-east-1" >>  conf/zeppelin-env.sh
+         echo "export ZEPPELIN_SPARK_ENABLESUPPORTEDVERSIONCHECK=false" >> conf/zeppelin-env.sh
+         echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.362.b09-2.el9_1.x86_64/jre" >> conf/zeppelin-env.sh
+         echo "export SPARK_MASTER=spark://10.1.1.93:7077" >> conf/zeppelin-env.sh
 
-         echo "export ZEPPELIN_ADDR=10.1.1.93" >> conf/zeppeline-env.sh
-         echo "export ZEPPELIN_PORT=8082" >> conf/zeppeline-env.sh
-         echo "export ZEPPELIN_LOCAL_IP=10.1.1.9" >> conf/zeppeline-env.sh
-         echo "export SPARK_HOME=/usr/lib/x14/spark/spark-3.3.2-bin-hadoop3" >> conf/zeppeline-env.sh
+         echo "export ZEPPELIN_ADDR=10.1.1.93" >> conf/zeppelin-env.sh
+         echo "export ZEPPELIN_PORT=8082" >> conf/zeppelin-env.sh
+         echo "export ZEPPELIN_LOCAL_IP=10.1.1.93" >> conf/zeppelin-env.sh
+         echo "export SPARK_HOME=/usr/lib/x14/spark/spark-3.3.2-bin-hadoop3" >> conf/zeppelin-env.sh
 
          cp /tmp/zeppelin.service /etc/systemd/system/zeppelin.service
          systemctl enable zeppelin.service
