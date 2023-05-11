06f7c575a132931e65facf244b158a89a33f4951
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-05-11 19:31:16 +0200

    Update install.sh

--- dist/rocky_9_1/installation_kits/mesh/install.sh
+++ dist/rocky_9_1/installation_kits/mesh/install.sh
@@ -17,7 +17,7 @@ h=$(hostname -s)
 interface=interfaces${h: -1}
 
 $brokkr_gotplexe ${interface}eth2.nmconnection --set eth0=${brokkr_mesh_interface_name["eth0"]} --set eth1=${brokkr_mesh_interface_name["eth1"]} --set eth2=${brokkr_mesh_interface_name["eth2"]} --set eth3=${brokkr_mesh_interface_name["eth3"]} > /etc/NetworkManager/system-connections/${interface}eth2.nmconnection
-$brokkr_gotplexe ${interface}eth3.nmconnection --set eth0=${brokkr_mesh_interface_name["eth0"]} --set eth1=${brokkr_mesh_interface_name["eth1"]} --set eth2=${brokkr_mesh_interface_name["eth2"]} --set eth3=${brokkr_mesh_interface_name["eth3"]} > /etc/NetworkManager/system-connections/${interface}eth2.nmconnection
+$brokkr_gotplexe ${interface}eth3.nmconnection --set eth0=${brokkr_mesh_interface_name["eth0"]} --set eth1=${brokkr_mesh_interface_name["eth1"]} --set eth2=${brokkr_mesh_interface_name["eth2"]} --set eth3=${brokkr_mesh_interface_name["eth3"]} > /etc/NetworkManager/system-connections/${interface}eth3.nmconnection
 
 ifdown --force eth0
 ifup eth0
