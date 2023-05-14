050a9c81a5cd989477fc8427feecfc9ff5cf7fae
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-05-14 22:01:37 +0200

    Update install.sh

--- dist/ubuntu_22_04/installation_kits/nessie_iceberg/install.sh
+++ dist/ubuntu_22_04/installation_kits/nessie_iceberg/install.sh
@@ -12,7 +12,7 @@
    chown -R spark:x14 jars/
    
    echo 'spark.jars.packages=org.mariadb.jdbc:mariadb-java-client:3.1.3,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.3_2.12:0.58.1,org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.2.1,software.amazon.awssdk:sts:2.20.18,software.amazon.awssdk:s3:2.20.18,software.amazon.awssdk:url-connection-client:2.20.18' >> conf/spark-defaults.conf
-   echo 'spark.sql.extensions="org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions"' >> conf/spark-defaults.conf
+   echo 'spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.uri=http://localhost:19120/api/v1' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.ref=main' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.catalog-impl=org.apache.iceberg.nessie.NessieCatalog' >> conf/spark-defaults.conf
@@ -20,6 +20,7 @@
    echo 'spark.sql.catalog.nessie=org.apache.iceberg.spark.SparkCatalog' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.io-impl=org.apache.iceberg.aws.s3.S3FileIO' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.s3.endpoint=http://locahost:9000' >> conf/spark-defaults.conf
+   echo 'spark.sql.catalog.nessie.s3.path-style-access=true' >> conf/spark-defaults.conf
    echo 'spark.sql.defaultCatalog=nessie' >> conf/spark-defaults.conf
 
    echo 'AWS_ACCESS_KEY_ID=labb' >> etc/env
