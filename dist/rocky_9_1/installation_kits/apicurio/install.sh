3102329d8c217e0a70a748fc0b693287246f8a62
root <root@r9v1.x14.se>
2023-05-29 00:41:05 +0200

    apicurio

---
+++ dist/rocky_9_1/installation_kits/apicurio/install.sh
@@ -0,0 +1,35 @@
+cp spark.env /tmp
+
+#yum install -y java-11-openjdk-headless zip
+#useradd -s /sbin/nologin -M kafka -G x14
+
+pushd /usr/lib/x14
+mkdir -p /var/lib/x14/kafka/apicurio
+chown -R kafka:x14 /var/lib/x14/kafka/apicurio
+
+pushd kafka
+git clone https://github.com/Apicurio/apicurio-registry.git
+
+pushd apicurio-registry
+
+
+mesh_machine_nr
+nr=$?
+
+./mvnw clean install -Pprod -Pkafkasql -DskipTests
+#storage/kafkasql/target/apicurio-registry-storage-kafkasql-2.4.3-SNAPSHOT.jar
+
+popd
+popd
+
+chown -R kafka:x14 apicurio
+popd
+
+if [[ ${nr} -eq 1 ]]
+then 
+    echo "I AM MASTER_HOST=${brokkr_mesh_ip[1]}"
+fi
+
+mv -f apicurio.service /etc/systemd/system/
+systemctl enable apicurio
+
