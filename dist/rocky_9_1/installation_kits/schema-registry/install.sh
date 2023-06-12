9b9d67841964ff81a9e2dfe0d5fc69a30592f553
root <root@r9v1.x14.se>
2023-06-12 02:27:34 +0200

    schema reg added

--- dist/rocky_9_1/installation_kits/schema-registry/install.sh
+++ dist/rocky_9_1/installation_kits/schema-registry/install.sh
@@ -5,9 +5,11 @@ git clone https://github.com/confluentinc/schema-registry.git
 cd schema-registry
 git checkout tags/v7.4.0 -b latest2
 mvn compile package -P standalone -DskipTests
+mkdir /usr/lib/x14/kafka/schema-registry/
+chown kafka:x14 /usr/lib/x14/kafka/schema-registry/
 mv -f package-schema-registry/target/kafka-schema-registry-package-7.4.0-standalone.jar /usr/lib/x14/kafka/schema-registry/
 echo "kafkastore.bootstrap.servers=10.1.1.93:9092" > schema-registry.json
-echo "listeners=http://localhost:8084" > schema-registry.json
+echo "listeners=http://10.1.1.93:8084" > schema-registry.json
 
 
 
