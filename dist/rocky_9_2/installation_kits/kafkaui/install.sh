f08af4bd704a1ace0279948c38cbbf53e6672055
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-07-18 01:15:27 +0200

    Update install.sh

--- dist/rocky_9_2/installation_kits/kafkaui/install.sh
+++ dist/rocky_9_2/installation_kits/kafkaui/install.sh
@@ -3,6 +3,7 @@ yum install -y java-17-openjdk-devel.x86_64
 
 cp kafkaui.service /tmp
 mkdir /usr/lib/x14/kafka/kafkaui
+mkdir /var/lib/x14/kafka/kafkaui
 pushd /usr/lib/x14/kafka/kafkaui
 #git clone https://github.com/provectus/kafka-ui.git
 #cd kafkaui
@@ -12,6 +13,8 @@ wget https://github.com/provectus/kafka-ui/releases/download/v0.7.1/kafka-ui-api
 mv -f /tmp/kafkaui.serve /etc/systemd/system/
 mv kafkaui.service /etc/systemd/system
 chown -R kafka:x14 /usr/lib/x14/kafka/kafkaui
+chown -R kafka:x14 /var/lib/x14/kafka/kafkaui
+
 systemctl enable kafkaui
 systemctl start kafkaui
 popd
