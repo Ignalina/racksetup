061485243c010fbcd61d6278f10026612fd938f0
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-07-18 01:17:10 +0200

    Update install.sh

--- dist/rocky_9_2/installation_kits/kafkaui/install.sh
+++ dist/rocky_9_2/installation_kits/kafkaui/install.sh
@@ -2,6 +2,8 @@
 yum install -y java-17-openjdk-devel.x86_64
 
 cp kafkaui.service /tmp
+cp application-local.yml /tmp
+
 mkdir /usr/lib/x14/kafka/kafkaui
 mkdir /var/lib/x14/kafka/kafkaui
 pushd /usr/lib/x14/kafka/kafkaui
@@ -10,8 +12,9 @@ pushd /usr/lib/x14/kafka/kafkaui
 #./mvnw install -DskipTests=true
 wget https://github.com/provectus/kafka-ui/releases/download/v0.7.1/kafka-ui-api-v0.7.1.jar
 #/usr/lib/jvm/java-17-openjd/java -Dspring.config.additional-location=application-local.yml  --add-opens java.rmi/javax.rmi.ssl=ALL-UNNAMED -jar kafka-ui-a>
-mv -f /tmp/kafkaui.serve /etc/systemd/system/
-mv kafkaui.service /etc/systemd/system
+mv -f /tmp/kafkaui.service /etc/systemd/system/
+mv -f /tmp/application-local.yml .
+#mv kafkaui.service /etc/systemd/system
 chown -R kafka:x14 /usr/lib/x14/kafka/kafkaui
 chown -R kafka:x14 /var/lib/x14/kafka/kafkaui
 
