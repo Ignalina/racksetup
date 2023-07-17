11252b78ba278e530cfeb3ee31aca2b4c9851b6c
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-07-18 01:12:15 +0200

    Update install.sh

--- dist/rocky_9_2/installation_kits/kafkaui/install.sh
+++ dist/rocky_9_2/installation_kits/kafkaui/install.sh
@@ -11,7 +11,7 @@ wget https://github.com/provectus/kafka-ui/releases/download/v0.7.1/kafka-ui-api
 #/usr/lib/jvm/java-17-openjd/java -Dspring.config.additional-location=application-local.yml  --add-opens java.rmi/javax.rmi.ssl=ALL-UNNAMED -jar kafka-ui-a>
 mv -f /tmp/kafkaui.serve /etc/systemd/system/
 mv kafkaui.service /etc/systemd/system
-chmown -R kafka:x14 /usr/lib/x14/kafka/kafkaui
+chown -R kafka:x14 /usr/lib/x14/kafka/kafkaui
 systemctl enable kafkaui
 systemctl start kafkaui
 popd
