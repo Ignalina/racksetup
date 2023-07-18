7377c4238034f68502021b1b9d530764f5e09ae7
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2023-07-18 17:30:06 +0200

    Update install.sh

--- dist/rocky_9_2/installation_kits/schema-registry/install.sh
+++ dist/rocky_9_2/installation_kits/schema-registry/install.sh
@@ -7,10 +7,11 @@ git checkout tags/v7.4.0 -b latest2
 mvn compile package -P standalone -DskipTests
 mkdir /usr/lib/x14/kafka/schema-registry/
 mv -f package-schema-registry/target/kafka-schema-registry-package-7.4.0-standalone.jar /usr/lib/x14/kafka/schema-registry/
-
-popd
 echo "kafkastore.bootstrap.servers=10.1.1.93:9092" > schema-registry.json
 echo "listeners=http://10.1.1.93:8084" >> schema-registry.json
+mv -f schema-registry.json /usr/lib/x14/kafka/schema-registry/
+popd
+
 chown -R kafka:x14 /usr/lib/x14/kafka/schema-registry/
 mv schema-registry.service /etc/systemd/system/
 systemctl enable schema-registry
