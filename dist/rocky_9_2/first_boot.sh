2e6a718ef926d6cdb9d9fefd8106eea05aae3b70
Rickard Lundin <rickard.lundin@enkla.com>
2023-08-21 13:18:55 +0200

    fix path

--- dist/rocky_9_2/first_boot.sh
+++ dist/rocky_9_2/first_boot.sh
@@ -2,14 +2,16 @@
 # TODO configurable x14 project/companyname
 x14scripts=$(realpath installation_kits/x14scripts)
 config=$(realpath default_config)
+PATH=${PATH}:${x14scripts}
+echo ${PATH}
 
-${x14scripts}/install_app.sh init ${config} 
-${x14scripts}/install_app.sh mellanox ${config}
-#./install_app.sh mesh ${config}
-${x14scripts}/install_app.sh datadisk ${config}
+install_app.sh init ${config}
+install_app.sh mellanox ${config}
+##install_app.sh mesh ${config}
+install_app.sh datadisk ${config}
 
 init 1
-${x14scripts}/install_app.sh createvar ${config}
+install_app.sh createvar ${config}
 reboot
 
 
