585aecaeac3b87cf1bf7622e5f0db1243028fda8
root <root@v1.ignalina.dk>
2023-11-06 01:17:33 +0100

    second boot

---
+++ dist/rocky_9_2/second_boot.sh
@@ -0,0 +1,14 @@
+# before cloning and running git is needed by "sudo yum install -y nano git git-lfs"
+# TODO configurable x14 project/companyname
+x14scripts=$(realpath installation_kits/x14scripts)
+config=$(realpath default_config)
+PATH=${PATH}:${x14scripts}
+echo ${PATH}
+
+install_app.sh nginx ${config}
+install_app.sh singleminio ${config}
+install_app.sh spark ${config}
+install_app.sh delta ${config}
+
+
+
