031ca07d1192d30e91b205f2cd6461c1fc31e26c
Rickard Lundin <rickard.lundin@enkla.com>
2024-01-22 23:29:39 +0100

    fix firewalld disabled

--- dist/rocky_9_3/installation_kits/init/install.sh
+++ dist/rocky_9_3/installation_kits/init/install.sh
@@ -15,7 +15,7 @@ chown -R x14:x14 /usr/lib/x14
 mkdir -p /var/lib/x14
 chown -R x14:x14 /var/lib/x14
 
-systemctl stop firewalld.service
-systemctl disable firewalld.service
+systemctl stop firewalld
+systemctl disable firewalld
 
 mesh_gen_hosts
