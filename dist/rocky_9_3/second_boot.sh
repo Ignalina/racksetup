b0998f995f7484f27bd2fc2da18d5eccc2f7430b
Rickard Lundin <rickard@ignalina.dk>
2024-01-23 23:48:12 +0100

    initiall support for kyuubi authz

--- dist/rocky_9_3/second_boot.sh
+++ dist/rocky_9_3/second_boot.sh
@@ -6,9 +6,10 @@ PATH=${PATH}:${x14scripts}
 echo ${PATH}
 
 install_app.sh nginx ${config}
-#install_app.sh singleminio ${config}
-#install_app.sh spark ${config}
-#install_app.sh delta ${config}
+install_app.sh singleminio ${config}
+install_app.sh spark ${config}
+install_app.sh delta ${config}
+install_app.sh kyuubi ${config}
 install_app.sh ranger ${config}
 
 
