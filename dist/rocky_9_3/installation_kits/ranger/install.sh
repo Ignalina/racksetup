7749bc2bb2be0ad872edfe04e6e0a3c784582efb
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2024-01-24 12:48:05 +0100

    Update install.sh

--- dist/rocky_9_3/installation_kits/ranger/install.sh
+++ dist/rocky_9_3/installation_kits/ranger/install.sh
@@ -8,11 +8,21 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m"
 mvn  -DskipJSTests clean compile package install
+
 popd
 
 
 mkdir -p /usr/lib/x14/ranger
 mkdir -p /var/lib/x14/ranger
+cp apache-ranger-2.4.0/target/ranger-2.4.0-admin.tar.gz .
+tar -xvf ranger-2.4.0-admin.tar.gz
+
+
+# Fetch mssql connector
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.26.tar.gz
+tar -xvf mysql-connector-java-8.0.26.tar.gz
+mv mysql-connector-java-8.0.26/mysql-connector-java-8.0.26.jar mysql-connector-java.jar
+
 
 groupadd -r ranger
 useradd -M -r -g ranger ranger
