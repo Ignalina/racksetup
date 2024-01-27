5c95b57ec42ca7e3a2f12487384b3bded42f43ca
Rickard Lundin <rickard@ignalina.dk>
2024-01-28 00:14:53 +0100

    solr 8.11.2 addd for ranger

---
+++ dist/rocky_9_3/installation_kits/solr/install.sh
@@ -0,0 +1,31 @@
+pushd /tmp
+yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
+
+wget https://www.apache.org/dyn/closer.lua/lucene/solr/8.11.2/solr-8.11.2.tgz?action=download -O solr.tar.gz
+mkdir /usr/lib/x14/solr/solr-8.11.2
+mkdir /var/lib/x14/solr/solr-8.11.2
+tar -zxf solr.tar.gz -C  /usr/lib/x14/solr
+popd
+
+
+pushd /usr/lib/x14/solr/solr-8.11.2
+
+echo "SOLR_PID_DIR=\"/var/lib/x14/solr/solr-8.11.2\"
+SOLR_INCLUDE=\"/usr/lib/x14/solr/solr-8.11.2/solr.in.sh\"
+SOLR_HOME=\"/var/lib/x14/solr/solr-8.11.2/data\"
+LOG4J_PROPS=\"/var/lib/x14/solr/solr-8.11.2/log4j2.xml\"
+SOLR_LOGS_DIR=\"/var/lib/x14/solr/solr-8.11.2/logs\"
+SOLR_PORT=\"8983\"
+JAVA_HOME=\"/usr/lib/jvm/java-1.8.0-openjdk\"
+PATH=\"/usr/lib/jvm/java-1.8.0-openjdk/bin:$PATH\"
+" >> "/usr/lib/x14/solr/solr-8.11.2/bin/solr.in.sh"
+popd
+
+useradd -M -r -g x14 solr
+chown -R solr:x14 /var/lib/x14/solr
+chown -R solr:x14 /usr/lib/x14/solr
+
+cp solr.service /etc/systemd/system/
+systemctl enable solr
+
+systemctl start solr.service
