102677fbce61f07579d134e625bc41bca067675d
Rickard Lundin <rickard@ignalina.dk>
2024-01-28 14:00:07 +0100

    zookeeper

--- dist/rocky_9_3/installation_kits/zk/install.sh
+++ dist/rocky_9_3/installation_kits/zk/install.sh
@@ -36,9 +36,7 @@ cp zookeeper.service /etc/systemd/system/
 systemctl enable zookeeper.service
 systemctl start zookeeper.service
 
-bin/zkCli.sh -server localhost:2181
-
-while ! /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/zkCli.sh -server localhost:2181 'sleep 5'; do  sleep 5; done ;
+while ! /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/bin/zkCli.sh -server localhost:2181 'sleep 5'; do  sleep 5; done ;
 
 echo "got passed zkcli localhost:2181"
 
