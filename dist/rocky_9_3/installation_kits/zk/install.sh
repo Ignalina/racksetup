d9ded184b4db8fe72a15c1cb2f66f639767b7b50
Rickard Lundin <rickard@ignalina.dk>
2024-01-28 02:07:32 +0100

    zk

---
+++ dist/rocky_9_3/installation_kits/zk/install.sh
@@ -0,0 +1,30 @@
+
+pushd /tmp
+
+wget https://dlcdn.apache.org/zookeeper/zookeeper-3.8.3/apache-zookeeper-3.8.3-bin.tar.gz -O zk.tar.gz
+mkdir -p /usr/lib/x14/zookeeper/
+mkdir -p /var/lib/x14/zookeeper/
+
+tar -zxf zk.tar.gz -C /usr/lib/x14/zookeeper
+rm -rf /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo_sample.cfg
+popd
+
+
+echo "tickTime=2000" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "dataDir=/var/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/ranger" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "clientPort=218" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+
+echo "initLimit=5" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "syncLimit=2" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "server.1=zoo1:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "server.2=zoo2:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "server.3=zoo3:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "autopurge.snapRetainCount=3" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "autopurge.purgeInterval=1" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+
+
+
+
+adduser -m -r -g x14 zookeeper
+chown -R zookeeper:x14 /usr/lib/x14/zookeeper
+chown -R zookeeper:x14 /var/lib/x14/zookeeper
