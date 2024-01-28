85e6a9de939663c84e72dbe054d3a8c02027f7f7
Rickard Lundin <rickard@ignalina.dk>
2024-01-28 09:44:20 +0100

    fix timeout

--- dist/rocky_9_3/installation_kits/zk/install.sh
+++ dist/rocky_9_3/installation_kits/zk/install.sh
@@ -7,6 +7,7 @@ mkdir -p /var/lib/x14/zookeeper/
 
 tar -zxf zk.tar.gz -C /usr/lib/x14/zookeeper
 rm -rf /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo_sample.cfg
+rm -rf zk.tar.gz
 popd
 
 
@@ -15,6 +16,7 @@ nr=$?
 
 echo "tickTime=2000" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 echo "dataDir=/var/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/ranger" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo $nr > /var/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/ranger/myid
 echo "clientPort=218" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 
 echo "initLimit=5" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
@@ -24,7 +26,6 @@ echo "server.2=${brokkr_mesh_ip[2]}:2888:3888" >> /usr/lib/x14/zookeeper/apache-
 echo "server.3=${brokkr_mesh_ip[3]}:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 echo "autopurge.snapRetainCount=3" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 echo "autopurge.purgeInterval=1" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
-
 
 
 adduser -m -r -g x14 zookeeper
