54006e10c9db0a588f573ce69a8d98d5e3c3ef65
Rickard Lundin <rickard@ignalina.dk>
2024-01-28 02:18:28 +0100

    zookeeper

--- dist/rocky_9_3/installation_kits/zk/install.sh
+++ dist/rocky_9_3/installation_kits/zk/install.sh
@@ -16,15 +16,18 @@ echo "clientPort=218" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/
 
 echo "initLimit=5" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 echo "syncLimit=2" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
-echo "server.1=zoo1:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
-echo "server.2=zoo2:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
-echo "server.3=zoo3:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "server.1=${brokkr_mesh_ip[1]}:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "server.2=${brokkr_mesh_ip[2]}:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
+echo "server.3=${brokkr_mesh_ip[3]}:2888:3888" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 echo "autopurge.snapRetainCount=3" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
 echo "autopurge.purgeInterval=1" >> /usr/lib/x14/zookeeper/apache-zookeeper-3.8.3-bin/conf/zoo.cfg
-
 
 
 
 adduser -m -r -g x14 zookeeper
 chown -R zookeeper:x14 /usr/lib/x14/zookeeper
 chown -R zookeeper:x14 /var/lib/x14/zookeeper
+
+cp zookeeper.service /etc/systemd/system/
+systemctl enable zookeeper.service
+systemctl start zookeeper.service
