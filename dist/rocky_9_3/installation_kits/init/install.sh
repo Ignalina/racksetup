63636bbba68b813719de8f4dfc8771edd8b3af35
Rickard Lundin <rickard@ignalina.dk>
2024-01-28 20:43:42 +0100

    fix net

--- dist/rocky_9_3/installation_kits/init/install.sh
+++ dist/rocky_9_3/installation_kits/init/install.sh
@@ -25,15 +25,12 @@ mesh_gen_hosts
 ### Set hostname and ip number
 systemctl stop NetworkManager
 
-#rm -rf eth0.nmconnection
-
-
 h=$(hostname -s)
 h_number=${h: -1}
-h_interface=${brokkr_ethext_interface_name[${h_number}]}
-h_ip=${brokkr_ethext_interface_ip[${h_number}]}
+h_interface=${brokkr_net_interface_name["eth0"]}
+h_ip=${brokkr_ethext_ip[${h_number}]}
 
-$brokkr_gotplexe eth0.nmconnection --set ethext_gatway=${brokkr_ethext_gateway}  --set eth0=${brokkr_net_interface_name["eth0"]} --set eth0_ip=${h_ip} > /etc/NetworkManager/system-connections/${brokkr_net_interface_name["eth0"]}.nmconnection
+$brokkr_gotplexe eth0.nmconnection --set ethext_gateway=${brokkr_ethext_gateway}  --set eth0=${brokkr_net_interface_name["eth0"]} --set eth0_ip=${h_ip} > /et>
 
 #systemctl start NetworkManager
 #nmcli connection reload
