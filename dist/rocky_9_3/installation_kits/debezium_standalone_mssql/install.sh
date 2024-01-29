595bbcb7b6f45aab412ce9efe546a56e9558f418
Rickard Lundin <rickard@ignalina.dk>
2024-01-29 19:44:08 +0100

    fixed debezium

--- dist/rocky_9_3/installation_kits/debezium_standalone_mssql/install.sh
+++ dist/rocky_9_3/installation_kits/debezium_standalone_mssql/install.sh
@@ -7,7 +7,7 @@ pushd /tmp
 
 wget https://repo1.maven.org/maven2/io/debezium/debezium-connector-mysql/2.5.0.Final/debezium-connector-mysql-2.5.0.Final-plugin.tar.gz -O  debezium-connector-mysql.tar.gz
 mkdir -p /usr/lib/x14/debezium_standalone
-tar -zxf debezium-connector-mysql.tar.gz
+tar -zxf debezium-connector-mysql.tar.gz -C /usr/lib/x14/debezium_standalone
 
 
 popd
