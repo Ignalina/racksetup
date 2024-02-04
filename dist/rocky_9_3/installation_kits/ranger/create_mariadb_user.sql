0e23f0aea8ba60409ed0e943f755f0ea5caae013
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2024-02-04 19:55:15 +0100

    Update create_mariadb_user.sql

--- dist/rocky_9_3/installation_kits/ranger/create_mariadb_user.sql
+++ dist/rocky_9_3/installation_kits/ranger/create_mariadb_user.sql
@@ -1,3 +1,7 @@
 CREATE USER 'ranger'@'%' IDENTIFIED BY 'changeme';
 GRANT ALL PRIVILEGES ON *.* TO 'ranger'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'%' IDENTIFIED BY 'changeme';
+flush privileges;
+exit;
