a25bd1f277097f3b22c0b7ed5444830d8cc51983
Rickard Lundin <rickard@ignalina.dk>
2024-02-04 16:26:26 +0100

    testing

--- dist/rocky_9_3/first_boot.sh
+++ dist/rocky_9_3/first_boot.sh
@@ -4,7 +4,7 @@ config=$(realpath default_config)
 PATH=${PATH}:${x14scripts}
 echo ${PATH}
 
-#install_app.sh init ${config}
+install_app.sh init ${config}
 #install_app.sh mellanox ${config}
 #install_app.sh mesh ${config}
 #install_app.sh datadisk ${config}
