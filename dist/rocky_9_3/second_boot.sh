89b0c9fc10857e5f311b52ab080fc501d938ecc7
Rickard Ernst Björn Lundin <33436048+Ignalina@users.noreply.github.com>
2024-03-17 10:22:38 +0100

    Update second_boot.sh

--- dist/rocky_9_3/second_boot.sh
+++ dist/rocky_9_3/second_boot.sh
@@ -3,18 +3,9 @@
 x14scripts=$(realpath installation_kits/x14scripts)
 config=$(realpath default_config)
 PATH=${PATH}:${x14scripts}
-echo ${PATH}
 
-install_app.sh zk ${config}
-install_app.sh solr ${config}
-install_app.sh nginx ${config}
-install_app.sh admindb ${config}
-install_app.sh singleminio ${config}
-install_app.sh schema-registry ${config}
-install_app.sh spark ${config}
-install_app.sh delta ${config}
-install_app.sh kyuubi ${config}
-install_app.sh ranger ${config}
+packages=$(realpath packages.sh)
+source ${packages}
 
 
 
