b1d7ec5a07b6550c1e10234e025220df5f23475e
Rickard Lundin <rickard@ignalina.dk>
2024-06-02 18:38:19 +0200

    should wok

--- infra/okd414/launch_fcos.sh
+++ infra/okd414/launch_fcos.sh
@@ -18,7 +18,7 @@ fi
 #
 # create vm 
 #
-
+chmod a+xwr $PWD/example.ign
 IGNITION_CONFIG="$PWD/example.ign"
 IMAGE="$PWD/fedora-coreos-40.20240504.3.0-qemu.x86_64.qcow2"
 VM_NAME="fcos-test-01"
@@ -28,9 +28,6 @@ STREAM="stable"
 DISK_GB="10"
 # For x86 / aarch64,
 IGNITION_DEVICE_ARG=(--qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}")
-
-# For s390x / ppc64le,
-IGNITION_DEVICE_ARG=(--disk path="${IGNITION_CONFIG}",format=raw,readonly=on,serial=ignition,startup_policy=optional)
 
 # Setup the correct SELinux label to allow access to the config
 chcon --verbose --type svirt_home_t ${IGNITION_CONFIG}
