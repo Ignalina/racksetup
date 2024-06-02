f2c7a102e5c7063b9d44c68ca6b7069a9169ca9a
Rickard Lundin <rickard@ignalina.dk>
2024-06-02 20:15:08 +0200

    woks

--- infra/okd414/launch_fcos.sh
+++ infra/okd414/launch_fcos.sh
@@ -4,7 +4,7 @@
 function butane () {
   podman run --rm --interactive --security-opt label=disable --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release --pretty --strict $1 > $2
 }
-
+rm example.ign
 butane example.bu example.ign
 #
 # fetch image
@@ -18,7 +18,7 @@ fi
 #
 # create vm 
 #
-chmod a+xwr $PWD/example.ign
+#sudo chmod a+xwr $PWD/example.ign
 IGNITION_CONFIG="$PWD/example.ign"
 IMAGE="$PWD/fedora-coreos-40.20240504.3.0-qemu.x86_64.qcow2"
 VM_NAME="fcos-test-01"
@@ -28,11 +28,16 @@ STREAM="stable"
 DISK_GB="10"
 # For x86 / aarch64,
 IGNITION_DEVICE_ARG=(--qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}")
+echo $IGNITION_DEVICE_ARG
 
 # Setup the correct SELinux label to allow access to the config
-chcon --verbose --type svirt_home_t ${IGNITION_CONFIG}
+#chcon --verbose --type svirt_home_t ${IGNITION_CONFIG}
 
 virt-install --connect="qemu:///system" --name="${VM_NAME}" --vcpus="${VCPUS}" --memory="${RAM_MB}" \
         --os-variant="fedora-coreos-$STREAM" --import --graphics=none \
         --disk="size=${DISK_GB},backing_store=${IMAGE}" \
-        --network bridge=virbr0 "${IGNITION_DEVICE_ARG[@]}"
+        --network bridge=virbr0 \
+        --sysinfo type="fwcfg,entry0.name=opt/com.coreos/config,entry0.file=${IGNITION_CONFIG}"
+#        "${IGNITION_DEVICE_ARG[@]}" \
+#        --sysinfo type="fwcfg,entry0.name=opt/com.coreos/config,entry0.file=${IGNITION_CONFIG}"
+#        --network bridge=virbr0 "${IGNITION_DEVICE_ARG[@]}" \
