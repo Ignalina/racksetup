b563685e3ed03f0417b13d9bed8031b86d1a1867
Rickard Lundin <rickard@ignalina.dk>
2024-06-05 17:29:12 +0200

    progress

--- infra/okd414_single_node/create_single_node_iso.sh
+++ infra/okd414_single_node/create_single_node_iso.sh
@@ -33,11 +33,16 @@ echo "pullSecret: '$(<pull-secret.txt)'" >> install-config.yaml
 echo "sshKey: |" >> install-config.yaml
 echo "  $(<~/.ssh/id_rsa.pub)" >> install-config.yaml
 
+echo "additionalTrustBundle: |" >> install-config.yaml
+cat ca.pem >> install-config.yaml
+
+
+
 rm -rf sno
 mkdir sno
 cp install-config.yaml sno
-./openshift-install --dir=sno create single-node-ignition-config
 ./openshift-install --dir=sno create manifests
+./openshift-install --dir=sno create single-node-ignition-config
 butane 99-worker-custom.bu -o sno/openshift/99-worker-custom.yaml
 
 coreos-installer iso ignition embed -fi sno/bootstrap-in-place-for-live-iso.ign fcos-live.iso
