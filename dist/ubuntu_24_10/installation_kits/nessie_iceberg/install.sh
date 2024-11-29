60385d65a940a463d227cd453a6e5f860d4dfdbd
Rickard Lundin <rickard.lundin@enkla.com>
2024-11-29 01:53:16 +0100

    newer

--- dist/ubuntu_24_10/installation_kits/nessie_iceberg/install.sh
+++ dist/ubuntu_24_10/installation_kits/nessie_iceberg/install.sh
@@ -8,10 +8,10 @@
    mkdir nessie
    pushd nessie
 
-   pushd /usr/lib/x14/spark/spark-3.3.2-bin-hadoop3/
+   pushd /usr/lib/x14/spark/spark-3.5.3-bin-hadoop3/
    chown -R spark:x14 jars/
    
-   echo 'spark.jars.packages=org.apache.hadoop:hadoop-aws:3.3.2,org.mariadb.jdbc:mariadb-java-client:3.1.3,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.3_2.12:0.58.1,org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.2.1,software.amazon.awssdk:sts:2.20.18,software.amazon.awssdk:s3:2.20.18,software.amazon.awssdk:url-connection-client:2.20.18' >> conf/spark-defaults.conf
+   echo 'spark.jars.packages=org.apache.hadoop:hadoop-aws:3.5.2,org.mariadb.jdbc:mariadb-java-client:3.1.3,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.5_2.12:0.99.0,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.7.0,software.amazon.awssdk:sts:2.29.1,software.amazon.awssdk:s3:2.29.1,software.amazon.awssdk:url-connection-client:2.29.1' >> conf/spark-defaults.conf
    echo 'spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.uri=http://localhost:19120/api/v1' >> conf/spark-defaults.conf
    echo 'spark.sql.catalog.nessie.ref=main' >> conf/spark-defaults.conf
@@ -38,8 +38,8 @@
 
       useradd -s /sbin/nologin -M nessie -G x14
 
-      wget https://github.com/projectnessie/nessie/releases/download/nessie-0.58.1/nessie-quarkus-0.58.1-runner
-      chmod +x nessie-quarkus-0.58.1-runner
+      wget https://github.com/projectnessie/nessie/releases/download/nessie-0.99.0/nessie-quarkus-0.99.0-runner
+      chmod +x nessie-quarkus-0.99.0-runner
       mkdir /var/lib/x14/nessie
       chown nessie:x14 /var/lib/x14/nessie
 
